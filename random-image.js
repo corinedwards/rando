cea0b2b67618c709bd1385b5cd78d7b33fd5c056
Corin Edwards <corinedwards@gmail.com>
2025-02-12 21:55:42 +1000

    added GitHub path for images

--- random-image.js
+++ random-image.js
@@ -2,6 +2,6 @@ document.addEventListener("DOMContentLoaded", function() {
   const randomNumber = Math.floor(Math.random() * 10); // 0 to 9
   const imgElement = document.getElementById('random-image');
   if (imgElement) {
-    imgElement.src = `img0${randomNumber}.png`;
+    imgElement.src = `https://raw.githubusercontent.com/corinedwards/rando/main/img0${randomNumber}.png`;
   }
 });
